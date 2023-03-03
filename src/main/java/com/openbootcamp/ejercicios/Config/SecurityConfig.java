26c68b6ddf3c521531c1a118186085509fff6a7b
sebastian <sebastiajhoan72@outlook.es>
2023-03-03 16:59:08 -0500

    organizando imports

--- src/main/java/com/openbootcamp/ejercicios/Config/SecurityConfig.java
+++ src/main/java/com/openbootcamp/ejercicios/Config/SecurityConfig.java
@@ -1,18 +1,15 @@
 package com.openbootcamp.ejercicios.Config;
 
-import lombok.RequiredArgsConstructor;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.security.authentication.AuthenticationManager;
 import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
-import org.springframework.security.core.userdetails.User;
-import org.springframework.security.core.userdetails.UserDetails;
-import org.springframework.security.core.userdetails.UserDetailsService;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.security.crypto.password.PasswordEncoder;
-import org.springframework.security.provisioning.InMemoryUserDetailsManager;
 import org.springframework.security.web.SecurityFilterChain;
+
+import lombok.RequiredArgsConstructor;
 
 /*
  * Security config es una clase que administra la gestion de autorizacion de endpoints
